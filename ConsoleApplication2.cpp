0dc522d33847fecf53c4891ab57effd77bb0e319
MyWork1988 <mywork1988@live.cn>
2018-10-18 21:33:07 +0800

    杭电oj

--- ConsoleApplication2.cpp
+++ ConsoleApplication2.cpp
@@ -275,6 +275,11 @@ int main() {
 	return 0;
 }
 */
+
+/*
+	title: 杭电2009 一种： c++
+*/
+/*
 #include <iostream>
 #include <Math.h>
 using namespace std;
@@ -289,6 +294,218 @@ int main() {
 		}
 		cout.precision(2);
 		cout << fixed << total << endl;
+	}
+	return 0;
+}
+*/
+
+/*
+	title: 杭电oj2010
+*/
+/*
+#include <iostream>
+#include <Math.h>
+using namespace std;
+
+int main() {
+	int num1, num2;
+	while (cin >> num1 >> num2 && 100<=num1 <= 999 && 100<=num2 <=999) {
+		int arr = [];
+		int temp;
+		int flag = 0;
+		if (num1 > num2) {
+			temp = num1;
+			num1 = num2;
+			num2 = temp;
+		}
+		for (int i = num1; i <= num2; i++) {
+			int s = i/100;
+			int e = (i - s * 100) / 10;
+			int y = i - s * 100 - e * 10;
+			if (s*s*s + e*e*e + y*y*y == i) {
+				flag++;
+				if (flag == 1)
+					cout << i;
+				else
+					cout << ' ' << i;
+			}
+			if (!flag)
+				cout << 'no';
+			cout << endl;
+				
+		}
+	}
+	return 0;
+}
+*/
+
+/*
+	title: 杭电2011 一种 c++
+*/
+
+/*
+#include <iostream>
+#include <Math.h>
+using namespace std;
+
+double algor(int num) {
+	double value = 0;
+	for (int i = 1; i <= num; i++) {
+		double m = 1.0/i;
+		if (i % 2 == 0)
+			m = -1*m;
+		value = value + m;
+	}
+	return value;
+}
+
+int main() {
+	int num1, num2, num3;
+	while (cin >>num1 && num1<100) {
+		for (int i = 0; i < num1; i++){
+			double value = 0;
+			bool flag = 1;
+			cin >> num2;
+			value = algor(num2);
+			if (flag) {
+				flag = 0;
+				cout.precision(2);
+				cout << fixed << value<<endl;
+			}
+			else {
+				cout.precision(2);
+				cout << fixed << value;
+			}
+		}
+		
+	}
+	return 0;
+}
+*/
+
+
+/*
+	title: 杭电2016  
+*/
+
+/*
+#include <iostream>
+#include <Math.h>
+using namespace std;
+
+int main() {
+	int num;
+	while (cin >> num) {
+		int *p=new int[num];
+		for (int i = 0; i < num; i++) {
+			
+		}
+	}
+	return 0;
+}
+*/
+
+
+/*
+#include <iostream>
+using namespace std;
+
+int main(){
+	int num;
+	while (cin >> num && num < 100){
+		int *p=new int[num];
+		int flag = 0;
+		for (int i = 0; i < num; i++){
+			cin >> p[i];
+			if (flag == 0){
+				p[0] == num1;
+				flag++;
+			}else{
+				if (num1 > p[0]){
+					
+				}
+			}
+		}
+	}
+	return 0;
+}
+
+/*
+	title： 杭电2016
+*/
+
+/*
+#include <iostream>
+using namespace std;
+
+int main()
+{
+int n,min,temp,j;
+while (cin>>n&&n!=0)
+{
+int *p=new int[n];//定义指针并指向用new动态分配大小为n的空间
+for (int i=0;i<n;i++)
+{
+cin>>p[i];
+if (i==0)
+{
+min=p[0];
+j=0;
+}
+else
+{
+if (p[i]<min)
+{
+min=p[i];
+j=i;
+}
+}
+
+}
+temp=p[0];
+p[0]=min;
+p[j]=temp;
+for (int i=0;i<n;i++)
+{
+if (i!=0)
+{
+cout<<" ";//数与数之间用空格分开
+}
+cout<<p[i];
+}
+cout<<endl;
+delete p;//记着delete释放空间
+}
+return 0;
+}
+
+*/
+
+
+
+/*
+	title：杭电2017
+*/
+
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	int num;
+	string s;
+	while (cin >> num) {
+		int total = 0;
+		for (int i = 0; i < num; i++) {
+			total = 0;
+			cin >> s;
+			for (int j = 0; j<s.size(); j++) {
+				if ((int)s[j] >= (int)'1' && (int)s[j] <= (int)'9')
+					total++;
+			}
+			cout << total << endl;
+		}
 	}
 	return 0;
 }
